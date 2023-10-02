606c5ce26288bee328485b24eed10af1d4b50bf4
Valexanderman <viktor.alexand@gmail.com>
2023-10-02 20:38:10 +0200

    added more flags and timeout

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - canonical mode / cooked mode - keyboard input only sent to terminal after enter is pressed 
 - raw mode - keyboard input is processed after each keypress
 
+Ctrl-S - Tells program to stop send outpup
+Ctrl-Q - resumes send output
+
+Ctrl-Z - run program in background
+fg to bring it pack
--- src/notebooc.c
+++ src/notebooc.c
@@ -15,17 +15,31 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = original_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    // c_iflag: input flag
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //IXON: Ct-s Ct-q ICRNL: Ct-m
+    // c_oflag: output flag
+    raw.c_oflag &= ~(OPOST);
+    // c_cflag: control flag
+    raw.c_cflag |= (CS8);
+    // c_lflag: local flag
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //ISIG: Ct-c Ct-z IEXTEN: Ct-v ICANON: canonical mode 
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
     enableRawMode();
 
-    char c;
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     while(read(STDIN_FILENO, &c, 1)==1 && c!= 'q') {
-        if (iscntrl(c)) printf("%d\n", c);
-        else printf("%d ('%c')\n", c, c);
+        if (iscntrl(c)) printf("%d\r\n", c);
+        else printf("%d ('%c')\r\n", c, c);
+        if (c == 'q') break;
     }
+    
     return 0;
 }
